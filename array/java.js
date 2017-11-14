7a86975dadfe9a2f28c577836a80d13aa38bb5fc
Vincent Garcia <swagabann@Vincents-MacBook-Pro.local>
2017-11-14 08:19:12 -0500

    safe place

---
+++ array/java.js
@@ -0,0 +1,47 @@
+var eveningstring = "morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning morning"
+
+console.log(eveningstring)
+
+var eveningArray = eveningstring.split(" ")
+console.log(eveningArray)
+
+
+var container = document.querySelector(".output ul")
+
+// for(var i = 0; i < nameArray.length; i++) {
+// 	console.log(nameArray[i])
+// }
+
+eveningArray.forEach(buildboxes);
+
+function buildboxes(individual, number) {
+	var box =document.createElement("div")
+	var caption =document.createElement("p")
+	box.classList.add("wrapper")
+	box.id = "evening" + number
+	caption.textContent = individual
+	box.appendChild(caption)
+	container.appendChild(box)
+}
+
+
+var list = document.querySelectorAll(".wrapper")
+
+console.log(list)
+
+
+list.forEach(listen)
+
+function listen(individual, number) {
+	el.addEventListener("mouseover", function(changecolor))
+}
+
+function changecolor(individual, number) {
+	
+	document.getElementById("element" + number).style.visibility="hidden"
+
+
+}
+
+
+
